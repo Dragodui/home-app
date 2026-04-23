b7215183ade683032d014c957b41badf9c62aa35
dragodui <alghazaliaks123@gmail.com>
2026-04-23 14:44:17 +0200

    Refactor code structure for improved readability and maintainability

--- client/public/sw.js
+++ client/public/sw.js
@@ -2,12 +2,7 @@ const CACHE_NAME = "household-manager-v2";
 const SHELL_URLS = [
   "/",
   "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
-  "/icons/manifest-icon-192.maskable.png",
-  "/icons/manifest-icon-512.maskable.png",
-  "/icons/favicon-196.png",
-  "/icons/apple-icon-180.png",
+  "/icons/icon.png",
 ];
 
 // Install: pre-cache the app shell
